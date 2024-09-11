265ecfabf0264844ba9964e34fe70dc476275437
Iman Bajalan <bajalaniman@gmail.com>
2024-09-11 22:05:02 +0200

    Solving the error number 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "404.html", // Path to the source file (404.html in root)
+          dest: "", // Destination folder inside 'dist'
+        },
+      ],
+    }),
+  ],
   base: "/",
 });
